03606d01cb6ee75d179a1366f5f9cebdc856c23a
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-27 12:50:44 +0530

    Push new project

---
+++ AdvanceConcepts/AdvanceConcepts/FuncDel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace AdvanceConcepts
+{
+    //  This program demo. how to declare Func Delegate.
+    //  This is generic in-built delegate.
+    //  It is like Action delegate but it returns a value
+    //  Out param must be a last param in list.
+    class FuncDel
+    {
+        public static int Add(int firstNum,int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        static void Main()
+        {
+            Func<int, int, int> func = Add;
+            Console.WriteLine(func(30,40));
+            Console.ReadLine();
+        }
+    }
+}
