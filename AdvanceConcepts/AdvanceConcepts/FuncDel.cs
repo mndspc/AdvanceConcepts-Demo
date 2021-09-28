cd1682a1844104ed0dfa6e008e5103f62b7284d5
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-09-28 12:36:35 +0530

    Push new Classes

--- AdvanceConcepts/AdvanceConcepts/FuncDel.cs
+++ AdvanceConcepts/AdvanceConcepts/FuncDel.cs
@@ -9,15 +9,26 @@ namespace AdvanceConcepts
     //  Out param must be a last param in list.
     class FuncDel
     {
+        public delegate TResult Func<in T1,in T2, out TResult>(int firstNum, int secondNum, out int result);
+
         public static int Add(int firstNum,int secondNum)
         {
             return firstNum + secondNum;
         }
-
+        public static int AddAndSub(int firstNum, int secondNum,out int result)
+        {
+            result = firstNum - secondNum;
+            return firstNum + secondNum;
+        }
         static void Main()
         {
-            Func<int, int, int> func = Add;
-            Console.WriteLine(func(30,40));
+
+            //Func<int, int, int> func = Add;
+            //Console.WriteLine(func(30,40));
+            int Result;
+            Func<int, int, int> func1 = AddAndSub;
+           Console.WriteLine($"The Add result is: {func1(20, 10, out Result)}");
+            Console.WriteLine($"The sub result is:{Result}");
             Console.ReadLine();
         }
     }
